c2db0b48829a8315c1302ae6827a425abc11ef34
Jordy de Ron <jordy@tronsoft.nl>
2026-08-31 21:23:36 +0200

    removed swaync

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -4,7 +4,6 @@
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd("wayle panel start")
-	hl.exec_cmd("swaync")
 	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 	hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
